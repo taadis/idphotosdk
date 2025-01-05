ec3962b6476c3a9def02a1b1abf456d8893a2741
taadis <i@taadis.com>
2025-01-05 14:38:46 +0800

    impl client.Idphoto func

---
+++ idphoto.go
@@ -0,0 +1,149 @@
+package idphotosdk
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"mime/multipart"
+	"net/http"
+	"reflect"
+	"time"
+)
+
+type IdphotoRequest struct {
+	InputImage         io.Reader `json:"-"`
+	InputImageBase64   string    `json:"-"`
+	Height             *int      `json:"height"`
+	Width              *int      `json:"width"`
+	HumanMattingModel  *string   `json:"human_matting_model"`
+	FaceDetectModel    *string   `json:"face_detect_model"`
+	Hd                 bool      `json:"hd"`
+	Dpi                *int      `json:"dpi"`
+	FaceAlignment      bool      `json:"face_alignment"`
+	HeadHeightRatio    *float64  `json:"head_height_ratio"`
+	HeadMeasureRatio   *float64  `json:"head_measure_ratio"`
+	TopDistanceMin     *float64  `json:"top_distance_min"`
+	TopDistanceMax     *float64  `json:"top_distance_max"`
+	BrightnessStrength *float64  `json:"brightness_strength"`
+	ContrastStrength   *float64  `json:"contrast_strength"`
+	SharpenStrength    *float64  `json:"sharpen_strength"`
+	SaturationStrength *float64  `json:"saturation_strength"`
+}
+
+func (r *IdphotoRequest) Request(ctx context.Context) (*http.Request, error) {
+	if r == nil {
+		return nil, ErrRequestIsNil
+	}
+
+	method := http.MethodPost
+	url := "idphoto"
+
+	return request(ctx, method, url, r.InputImage, r.InputImageBase64, r)
+}
+
+func request(ctx context.Context, method string, url string, inputImage io.Reader, inputImageBase64 string, params interface{}) (*http.Request, error) {
+	if inputImage == nil && inputImageBase64 == "" {
+		return nil, errors.New("invalid input image or base64 is empty")
+	}
+
+	paramsBuf, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	var fields map[string]interface{}
+	err = json.Unmarshal(paramsBuf, &fields)
+	if err != nil {
+		return nil, err
+	}
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	// that's not work
+	// for k, v := range fields {
+	// 	err = writer.WriteField(k, fmt.Sprintf("%v", v))
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// }
+	// using reflect to write struct fields
+	v := reflect.ValueOf(params)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		fieldName := t.Field(i).Tag.Get("json")
+		if fieldName == "" || fieldName == "-" {
+			continue
+		}
+		fieldValue := v.Field(i).Interface()
+		if reflect.ValueOf(fieldValue).Kind() == reflect.Ptr && reflect.ValueOf(fieldValue).IsNil() {
+			continue
+		}
+		err = writer.WriteField(fieldName, fmt.Sprintf("%v", fieldValue))
+		if err != nil {
+			return nil, err
+		}
+		slog.Info("write field", "fieldName", fieldName, "fieldValue", fieldValue)
+	}
+
+	if inputImage != nil {
+		filename := fmt.Sprintf("%d.jpg", time.Now().UnixNano())
+		part, err := writer.CreateFormFile("input_image", filename)
+		if err != nil {
+			return nil, err
+		}
+		_, err = io.Copy(part, inputImage)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		err = writer.WriteField("input_image_base64", inputImageBase64)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, nil
+}
+
+func NewIdphotoRequest() *IdphotoRequest {
+	return &IdphotoRequest{}
+}
+
+type IdphotoResponse struct {
+	Status bool `json:"status"`
+
+	ImageBase64Standard string `json:"image_base64_standard,omitempty"`
+
+	ImageBase64Hd string `json:"image_base64_hd,omitempty"`
+}
+
+func (c *Client) Idphoto(ctx context.Context, req *IdphotoRequest) (*IdphotoResponse, error) {
+	return c.idphoto(ctx, req)
+}
+
+func (c *Client) idphoto(ctx context.Context, req *IdphotoRequest) (*IdphotoResponse, error) {
+	var rsp IdphotoResponse
+	err := c.Do(ctx, req, &rsp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rsp, nil
+}
---
+++ idphoto_test.go
@@ -0,0 +1,72 @@
+package idphotosdk
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestIdphoto(t *testing.T) {
+	ctx := context.Background()
+	client := NewClient(
+		WithBaseUrl(getHivisionIdphotoBaseUrl()),
+		WithLogEnabled(true),
+	)
+
+	t.Run("invalid_source", func(t *testing.T) {
+		req := NewIdphotoRequest()
+		_, err := client.Idphoto(ctx, req)
+		if err == nil {
+			t.Fatalf("want non-nil but got %v", err)
+		}
+		if err.Error() != "invalid input image or base64 is empty" {
+			t.Fatalf("want error msg but got:%v", err)
+		}
+	})
+
+	t.Run("with-input-image-standard", func(t *testing.T) {
+		req := NewIdphotoRequest()
+		req.InputImage = file
+		req.Hd = false
+		rsp, err := client.Idphoto(ctx, req)
+		if err != nil {
+			t.Fatalf("request failed, error:%v", err)
+		}
+		if rsp == nil {
+			t.Fatalf("want non-nil but got %v", rsp)
+		}
+		if rsp.Status != true {
+			t.Fatalf("want true but got %v", rsp.Status)
+		}
+		if rsp.ImageBase64Standard == "" {
+			t.Fatal("want image base64 standard but got empty")
+		}
+	})
+
+	t.Run("with-input-image-base64", func(t *testing.T) {
+		fileBytes, err := os.ReadFile(inputImagePath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		base64Str := base64.StdEncoding.EncodeToString(fileBytes)
+		base64Str = fmt.Sprintf("data:image/jpeg;base64,%s", base64Str)
+		req := NewIdphotoRequest()
+		req.InputImageBase64 = base64Str
+		rsp, err := client.Idphoto(ctx, req)
+		if err != nil {
+			t.Fatalf("request failed, error:%v", err)
+		}
+		if rsp == nil {
+			t.Fatalf("want non-nil but got %v", rsp)
+		}
+		if rsp.Status != true {
+			t.Fatalf("want true but got %v", rsp.Status)
+		}
+		if rsp.ImageBase64Standard == "" {
+			t.Fatal("want image base64 standard but got empty")
+		}
+	})
+
+}
